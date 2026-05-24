5d0f76bb1c192bed8956bbc3450f3b88d38cc224
Tien Long Lam <lamtienlong9@gmail.com>
2026-05-24 12:51:47 +1000

    test: consolidate common harness allowance

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,10 +1,7 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+
 pub mod cli;
-#[allow(dead_code)]
 pub mod fixtures;
-#[allow(dead_code)]
 pub mod helpers;
-#[allow(dead_code)]
 pub mod mcp;
-#[allow(dead_code)]
 pub mod provider_conformance;
